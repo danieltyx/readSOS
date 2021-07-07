02391806979708d1642a2c7eb23f4a19739bd023
danieltyx <danieltian.yx@gmail.com>
2021-07-08 00:43:48 +0800

    V1.0

---
+++ readSOS/readSOS/readSOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  readSOSApp.swift
+//  readSOS
+//
+//  Created by Daniel T. on 7/8/21.
+//
+
+import SwiftUI
+
+@main
+struct readSOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
